9d0cec9c379601bc971e865cfeb42ea43328d204
Udaykiran Reddy Tippireddy <udaykiranreddy0608@gmail.com>
2021-05-03 23:54:17 +0530

    Updated backend service

--- backend/settings.py
+++ backend/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',  
+    'todo',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Allow cross origin requests
+CORS_ORIGIN_ALLOW_ALL = True
