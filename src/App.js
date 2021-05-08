0083d9361d7c17d8674dc76feff076306330c957
Udaykiran Reddy Tippireddy <udaykiranreddy0608@gmail.com>
2021-05-08 15:05:20 +0530

    Added UI changes for todo app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,78 @@
-//import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import Modal from "./components/Modal";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <h1> Welcome to TO-DO App</h1>
-          <body className = "App-body">
-            <p>Please login to continue...</p>
-          </body>
-      </header>
-    </div>
-  );
-}
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+      todolist: []
+    }
+
+  }
+
+  refreshList = () =>{
+    axios.get("/api/todos/")
+    .then((res) => { this.setState({ todolist: res.data})})
+    .catch(err => console.log(err))
+  }
+  renderTabList = ()  =>{
+    return(
+
+      <div>
+        renderTabList
+      </div>
+    )
+  }
+
+  renderItemList = () =>{
+    
+    const todolist = this.state.todolist;
+    return todolist.map( item =>(
+
+      <li>{item.id}
+          
+          {item.title}
+          
+          {item.description}
+          
+          {item.completed}
+      </li>
+
+    )
+    
+    )
+  }
+
+  render() {
+          return (
+            <main className="content">
+            <h1 className="text-black text-center my-4"> Todo App</h1>
+            <div className="row">
+            <div className="col-md-6 col-sm-10 mx-auto p-0">
+            
+            <div className="card p-3">
+              <div class = "">
+                <button className="btn btn-primary"  style={{float: 'right'}} >Add task</button>
+              </div>
+              <div class = "">
+                <button className="btn btn-primary"  onClick = {()=>this.refreshList()} >Refresh</button>
+              </div>
+              {this.renderTabList()}
+              <ul>{this.renderItemList()}</ul>
+            </div>
+            </div>
+            </div>
+
+
+            </main>
+          );
+        }
+        }
 
 export default App;
