fb942ce92643508b99f899606a32e2e073590d5e
Udaykiran Reddy Tippireddy <udaykiranreddy0608@gmail.com>
2021-05-14 15:38:06 +0530

    Updated settings.py file

--- backend/settings.py
+++ backend/settings.py
@@ -138,7 +138,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, 'build/static'),
+    os.path.join(BASE_DIR, 'build/')
+
 ]
    
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- todo/views.py
+++ todo/views.py
@@ -6,6 +6,7 @@ from .models import Todo
 from django.views import View
 from django.http import HttpResponse, HttpResponseNotFound
 import os
+
 class TodoView(viewsets.ModelViewSet):      
     serializer_class = TodoSerializer       
     queryset = Todo.objects.all()      
