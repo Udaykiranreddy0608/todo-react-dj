c84ccf331db80727c17dda04261abe521ef1fed6
Udaykiran Reddy Tippireddy <udaykiranreddy0608@gmail.com>
2021-05-14 15:49:11 +0530

    Updated static dir

--- backend/settings.py
+++ backend/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static'),
-    os.path.join(BASE_DIR, 'build/')
+    os.path.join(BASE_DIR, 'build')
 
 ]
    
--- todo/views.py
+++ todo/views.py
@@ -10,4 +10,14 @@ import os
 class TodoView(viewsets.ModelViewSet):      
     serializer_class = TodoSerializer       
     queryset = Todo.objects.all()      
-   
+
+class Assets(View):
+
+    def get(self, _request, filename):
+        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                return HttpResponse(file.read(), content_type='application/javascript')
+        else:
+            return HttpResponseNotFound() 
